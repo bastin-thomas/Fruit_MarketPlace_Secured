95e02bf694655f3b40ebae606e03da800bcf6364
Thomas Bastin <thomasbastin366@gmail.com>
2024-01-09 23:35:46 +0100

    :memo: update readme

--- StockManagementWebServer/src/main/java/be/hepl/stockmanagement/HttpServer/StockManagement_HttpServer.java
+++ StockManagementWebServer/src/main/java/be/hepl/stockmanagement/HttpServer/StockManagement_HttpServer.java
@@ -39,12 +39,14 @@ public class StockManagement_HttpServer{
         http = HttpServer.create(addr, maxPending);
         
         Executor executor = Executors.newFixedThreadPool(nthread);
+        
         http.setExecutor(executor);
         
         this.log.Trace("Lancement du serveur HTTP sur les ports: " + ip + ":" + port);
         
         http.createContext("/", new IndexHandler(log, db));
         http.createContext("/api/v1/", new ApiHandler(log, db));
+        
         http.start();
         
         this.log.Trace("Serveur HTTP lancé sur les ports: " + ip + ":" + port);
